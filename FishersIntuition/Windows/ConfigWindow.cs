1537fbd84ba90f86a1747501e56543b568cf9669
decorwdyun <decorwdyun@gmail.com>
2025-09-17 22:30:50 +0800

    chore: Api 13 (#3)

--- FishersIntuition/Windows/ConfigWindow.cs
+++ FishersIntuition/Windows/ConfigWindow.cs
@@ -1,5 +1,5 @@
-﻿using Dalamud.Interface.Windowing;
-using ImGuiNET;
+﻿using Dalamud.Bindings.ImGui;
+using Dalamud.Interface.Windowing;
 using Dalamud.Interface.ImGuiFileDialog;
 
 namespace FishersIntuition.Windows;
